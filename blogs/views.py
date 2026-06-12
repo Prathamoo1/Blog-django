f2dc4675235198189675630b9308b7bc707677d5
Pratham Patidar <25it1pr103@mitsgwl.ac.in>
2026-06-12 22:34:55 +0530

    Single Blog page

--- blogs/views.py
+++ blogs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse
 from blogs.models import Blog ,Category
 
@@ -17,3 +17,10 @@ def posts_by_category(request,category_id):
         'category':category ,
     }
     return render(request,'posts_by_category.html',context)
+
+def blogs(request,slug):
+    single_blog=get_object_or_404(Blog,slug=slug,status="Published")
+    context={
+        'single_blog':single_blog,
+    }
+    return render(request,"blogs.html",context)
