26976fce6b4e20da5d98047c446d9054918a1894
Pratham Patidar <25it1pr103@mitsgwl.ac.in>
2026-06-14 22:31:42 +0530

    About Us section

---
+++ about/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class About(models.Model):
+    about_heading=models.CharField(max_length=50)
+    about_discription=models.TextField(max_length=200)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.about_heading
--- blog_main/views.py
+++ blog_main/views.py
@@ -1,12 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from blogs.models import Category,Blog
+from about.models import About
 
 def home(request):
     featured_post=Blog.objects.filter(is_featured=True,status='Published').order_by('updated_at')
     posts=Blog.objects.filter(is_featured=False,status='Published')
+    # featch About Us
+    try:
+        about=About.objects.get()
+    except:
+        about=None
     context={
         'featured_post':featured_post,
         'posts':posts,
+        'about':about,
     }
     return render(request,"home.html",context)
